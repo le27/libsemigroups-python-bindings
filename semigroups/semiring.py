104f6421ed4fe3db5f49ec61b287a2906b9bda13
Alex Levine <aal20@st-andrews.ac.uk>
2017-05-30 10:47:08 +0100

    Add SemiringWithThreshold and TropicalMaxPlusSemiring, together with some minor linting changes

--- semigroups/semiring.py
+++ semigroups/semiring.py
@@ -9,7 +9,7 @@ class Semiring:
     r"""
     A semiring is a set :math:`R`, together with two binary operations,
     :math:`+, \times`, such that :math:`(R, +)` is a commutative monoid,
-    with identity called 0, :math:`(R\backslash\{0\},\times)` is a 
+    with identity called 0, :math:`(R\backslash\{0\},\times)` is a
     monoid, with identity 1, :math:`(R, +, \times)` is left and right
     distributive (ie :math:`\forall a, b, c \in R \quad a(b + c) = ab + ac` and
     :math:`\forall a, b, c \in R \quad (a + b)c = ac + bc`), and multiplication
@@ -42,7 +42,6 @@ class Integers(Semiring):
 
     Raises:
         TypeError:  If any argument is given.
-        
     """
 
     def __init___(self):
@@ -62,7 +61,6 @@ class Integers(Semiring):
 
         Raises:
             TypeError:  If x and y are not both ints.
-            
         """
         if not (isinstance(x, int) and isinstance(y, int)):
             raise TypeError
@@ -101,7 +99,6 @@ class Integers(Semiring):
 
         Raises:
             TypeError:  If any argument is given.
-            
         """
         return 0
 
@@ -387,7 +384,7 @@ class BooleanSemiring(Semiring):
 
     def zero(self):
         """
-        A function to find the additive identity of the boolean semiring, which
+        A function to find the additive identity of the boolean  self._threshold = threshold semiring, which
         is False.
 
         Args:
@@ -416,4 +413,162 @@ class BooleanSemiring(Semiring):
             TypeError:  If any argument is given.
         """
         return True
+
+class SemiringWithThreshold(Semiring):
+    """
+    A semiring with a threshold is a semiring is a semiring with a largest
+    finite value, the threshold.
+
+    This abstract class provides common methods for its subclasses.
+
+    Returns:
+        None
+
+    Raises:
+        TypeError:  If any argument is given.
+    """
+    def __init__(self):
+        pass
+
+    def threshold(self):
+        """
+        A function to find the threshold of a semiring.
+
+        Returns:
+            The threshold of the semiring.
+
+        Raises:
+            TypeError:  If any argument is given.
+        """
+        return self._threshold
+
+class TropicalMaxPlusSemiring(SemiringWithThreshold):
+    r"""
+    The tropical max plus semiring is a semiring comprising the set :math:`\{0,
+    \ldots, t\} \cup\{-\infty\}`, for some value :math:`t\in\mathbb{N}_0`, the
+    threshold of the semiring, together with an operation which returns the
+    maximum of two elements, as the additive operation and addition of integers
+    as the multiplicative operation. 
+
+    Minus infinity is a defined as smaller than all integers, and the integer
+    sum of minus infinity and any element of the max plus semiring is minus
+    infinity.
+
+    If the integer sum of any two elements is greater than the threshold, then 
+    the product is the threshold.
+    
+    Args:
+        threshold (int):    The threshold of the semiring.
+
+    Returns:
+        None
+
+    Raises:
+        TypeError:  If threshold is not an int.
+        ValueError: If threshold is negative.
+        
+    """
+    def __init___(self, threshold):
+        if not isinstance(threshold, int):
+            raise TypeError
+        if threshold < 0:
+            raise ValueError
+        self._threshold = threshold
+
+    def plus(self, x, y):
+        """
+        A function to find the maximum of two elements of the tropical max plus
+        semiring, since this is the addition operation of the tropical max plus
+        semiring.
+
+        Args:
+            x (int or float):    One of the elements to be added.
+            y (int or float):    The other of the elements to be added.
+
+        Returns:
+            int or float:   The maximum of x and y.
+
+        Raises:
+            TypeError:  If x and y are not both ints or minus infinity.
+            ValueError: If either x or y is negative and not minus infinity, or
+                        if x or y is greater than the threshold.
+            
+        """
+
+        if not ((isinstance(x, int) or x == -float('inf'))
+                and (isinstance(y, int) or y == -float('inf'))):
+            raise TypeError
+
+        if (x < 0 and x != -float(inf)) or (y < 0 and y != -float(inf)):
+            raise ValueError
+
+        if (x > self._threshold) or (y > self._threshold):
+            raise ValueError
+    
+        return max(x, y)
+
+    def prod(self, x, y):
+        """
+        A function to find the integer sum of two elements of the tropical max
+        plus semiring, since this is the addition operation of the max plus
+        semiring. If the integer sum is greater than :math:`t`, then the result
+        is :math:`t`.
+
+        Args:
+            x (int or float):    One of the elements to be multiplied.
+            y (int or float):    The other of the elements to be multplied.
+
+        Returns:
+            int or float:    x + y
+
+        Raises:
+            TypeError:  If x and y are not both ints or minus infinity.
+            ValueError: If either x or y is negative and not minus infinity, or
+                        if x or y is greater than the threshold.
+        """
+
+        if not ((isinstance(x, int) or x == -float('inf'))
+                and (isinstance(y, int) or y == -float('inf'))):
+            raise TypeError
+
+        if (x < 0 and x != -float(inf)) or (y < 0 and y != -float(inf)):
+            raise ValueError
+
+        if (x > self._threshold) or (y > self._threshold):
+            raise ValueError
+
+        return min(self._threshold, x + y)
+
+    def zero(self):
+        """
+        A function to find the additive identity of the tropical max plus
+        semiring, which is minus infinity.
+
+        Args:
+            None
+
+        Returns:
+            float:   -inf
+
+        Raises:
+            TypeError:  If any argument is given.
+        """
+        return self._minus_infinity
+
+    def one(self):
+        """
+        A function to find the multiplicative identity of the tropical max plus
+        semiring, which is 0.
+
+        Args:
+            None
+
+        Returns:
+            int:    0
+
+        Raises:
+            TypeError:  If any argument is given.
+            
+        """
+        return 0
 
