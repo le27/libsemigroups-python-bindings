98b8ef1ca043ca4acb2bb6cd26c47a1b9165d8af
Alex Levine <aal20@st-andrews.ac.uk>
2017-06-02 12:55:05 +0100

    cayley_graph: Add __eq__, __ne__ and asserts

--- semigroups/cayley_graph.py
+++ semigroups/cayley_graph.py
@@ -15,10 +15,22 @@ class CayleyGraph:
         self._label_edge_list = []
         self._graph = networkx.classes.multidigraph.MultiDiGraph()
 
+    def __eq__(self, other):
+        return self.ordered_adjacencies() == other.ordered_adjacencies()
+
+    def __ne__(self, other):
+        return not self == other
+
     def _add_node(self, node):
         self._graph.add_node(node)
 
     def _add_edge_with_label(self, label, edge):
+        if not isinstance(edge, tuple):
+            raise TypeError('edge must be a tuple')
+        if not isinstance(label, int):
+            raise TypeError('label must be an int')
+        if label < 0:
+            raise ValueError('label must be non-negative')
         self._graph.add_edge(*edge)
         self._label_edge_list.append((label, edge))
 
