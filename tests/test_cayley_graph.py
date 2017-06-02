8d33cccba7a59f47c5100eba8ef6da58708265b8
Alex Levine <aal20@st-andrews.ac.uk>
2017-06-02 17:12:53 +0100

    test_cayley_graph: Add tests for nodes and edges methods

--- tests/test_cayley_graph.py
+++ tests/test_cayley_graph.py
@@ -79,5 +79,53 @@ class TestCayleyGraph(unittest.TestCase):
             S = FullTransformationMonoid(2)
             S.right_cayley_graph().strongly_connected_components(1)
 
+    def test_edges(self):
+        G = Semigroup(Transformation([0, 1, 1]), 
+                       Transformation([0, 0, 0])).right_cayley_graph()
+        L = G.edges()
+        self.assertEqual(L, [(0, 0), (0, 1), (1, 1), (1, 1)])
+
+        L = Semigroup(complex(0, 1)).right_cayley_graph().edges()
+        self.assertEqual(L, [(0, 1), (1, 2), (2, 3), (3, 0)])
+
+        G = FullTransformationMonoid(3).right_cayley_graph()
+        L = G.edges()
+        M = [(0, 3), (0, 1), (0, 4), (1, 5), (1, 1), (1, 6), (2, 7), (2, 8),
+             (2, 9), (3, 0), (3, 1), (3, 2), (4, 9), (4, 10), (4, 7), (5, 1),
+             (5, 1), (5, 11), (6, 12), (6, 12), (6, 13), (7, 2), (7, 8),
+             (7, 0), (8, 14), (8, 8), (8, 15), (9, 4), (9, 10), (9, 3),
+             (10, 16), (10, 10), (10, 17), (11, 13), (11, 18), (11, 12),
+             (12, 6), (12, 12), (12, 5), (13, 11), (13, 18), (13, 1), (14, 8),
+             (14, 8), (14, 19), (15, 20), (15, 20), (15, 21), (16, 10),
+             (16, 10), (16, 22), (17, 23), (17, 23), (17, 24), (18, 25),
+             (18, 18), (18, 26), (19, 21), (19, 18), (19, 20), (20, 15),
+             (20, 20), (20, 14), (21, 19), (21, 18), (21, 8), (22, 24),
+             (22, 18), (22, 23), (23, 17), (23, 23), (23, 16), (24, 22),
+             (24, 18), (24, 10), (25, 18), (25, 18), (25, 18), (26, 26),
+             (26, 26), (26, 25)]
+
+        self.assertEqual(L, M)
+
+        with self.assertRaises(TypeError):
+            S = FullTransformationMonoid(2)
+            S.right_cayley_graph().edges(1)
+
+    def test_nodes(self):
+        G = Semigroup(Transformation([0, 1, 1]), 
+                       Transformation([0, 0, 0])).right_cayley_graph()
+        L = G.nodes()
+        self.assertEqual(L, [0, 1])
+
+        L = Semigroup(complex(0, 1)).right_cayley_graph().nodes()
+        self.assertEqual(L, [0, 1, 2, 3])
+
+        G = FullTransformationMonoid(3).right_cayley_graph()
+        L = G.nodes()
+        self.assertEqual(L, list(range(27)))
+
+        with self.assertRaises(TypeError):
+            S = FullTransformationMonoid(2)
+            S.right_cayley_graph().edges(1)
+
 if __name__ == "__main__":
     unittest.main()
